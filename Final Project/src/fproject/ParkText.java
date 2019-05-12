0dd02559600b92338710a9752b0e96872261eba6
kfe11 <49003302+kfe11@users.noreply.github.com>
2019-05-12 19:15:47 -0400

    Update ParkText.java

--- Final Project/src/fproject/ParkText.java
+++ Final Project/src/fproject/ParkText.java
@@ -5,12 +5,12 @@ public class ParkText extends BedroomText{
 	private String text4;
 	
 	public ParkText() {
-		super.setText("do you want a donut (+20min) or a coffee(super speed but it cost more)?");
-		super.setText2("you walked the park but got lost, now you wont make it to work on time!");
-		super.setText3("The donut was great but it cost you 20 mins and 3$."
-				+ "\nIt is time to head to work. should you walk or take a taxi");
-		this.text4 = "The coffee cost you 8$ but gave you boost."
-				+ "\n It is now time to head to work. should you walk or take a taxi";
+		super.setText("Do you want a donut (+20min) or a coffee (super speed but it cost more)?");
+		super.setText2("You walked the park but got lost, now you won't make it to work on time!");
+		super.setText3("The donut was great but it cost you 20 mins and $3."
+				+ "\nIt is time to head to work. Should you walk or take a taxi?");
+		this.text4 = ("The coffee costs you $8, but it gave you boost."
+				+ "\n It is now time to head to work. Should you walk or take a taxi?");
 	}
 	
 	public String getText4() {
