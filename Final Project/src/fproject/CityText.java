0982e45dd8cd77ee6e347aec1a4953dac863236b
kfe11 <49003302+kfe11@users.noreply.github.com>
2019-05-12 19:13:11 -0400

    Update CityText.java

--- Final Project/src/fproject/CityText.java
+++ Final Project/src/fproject/CityText.java
@@ -4,12 +4,12 @@ public class CityText extends BedroomText {
 	private String text4;
 	
 	public CityText() {
-		super.setText("you are now in the city and you have to go to central park. Should you take a taxi, take the subway, or walk");
-		super.setText2( "you took a taxi but there was too much traffic and you wont make it to work on time");
-		super.setText3("you took the subway but there was a slight delay, it took you 15mins and it cost you $2.75."
-				+ "\nyou still have some time. do you want to get food or walk the park");
-		this.text4 = "you had peaceful walk to the park and you have some time"
-				+ "\ndo you want to get food or walk the park";
+		super.setText("You are now in the city and you have to go to Central Park. Should you take a taxi, take the subway, or walk?");
+		super.setText2( "You took a taxi but there was too much traffic and you wont make it to work on time,");
+		super.setText3("You took the subway but there was a slight delay, it took you 15mins and it cost you $2.75."
+				+ "\nYou still have some time. Do you want to get food or walk to the park?");
+		this.text4 = "You had peaceful walk to the park and you have some time."
+				+ "\nDo you want to get food or take a walk in the park?";
 	}
 	
 	public String getText4() {
