45c9955288b9ef53886605782968cdf2d10b0e4b
kfe11 <49003302+kfe11@users.noreply.github.com>
2019-05-12 19:18:53 -0400

    Update TrainText.java

--- Final Project/src/fproject/TrainText.java
+++ Final Project/src/fproject/TrainText.java
@@ -4,8 +4,8 @@ public class TrainText extends BedroomText {
 	
 	public TrainText() {
 		
-		super.setText("You took the bus to the NB Train Station. How do you want to get to the city:");
-		super.setText2("you took the train. you now have $16");
-		super.setText3("you took the bus. you now have $18.50");
+		super.setText("You took the bus to the NB Train Station. How do you want to get to the city: by train (costs $14) or by bus (costs $11.50)?");
+		super.setText2("You took the train. You now have $16.");
+		super.setText3("You took the bus. You now have $18.50.");
 	}
 }
