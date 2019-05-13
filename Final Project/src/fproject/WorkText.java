d768ade2b578f967fd922c74176eae5885ee533c
shaiesta <49007501+shaiesta@users.noreply.github.com>
2019-05-13 14:31:03 -0400

    Update WorkText.java

--- Final Project/src/fproject/WorkText.java
+++ Final Project/src/fproject/WorkText.java
@@ -8,7 +8,7 @@ public class WorkText extends BedroomText {
 		super.setText("Sorry, you won't make it in time because the donut slowed you down.");
 		super.setText2("You had just enough money left to take a taxi and you made it to work on time! Congrats!");
 		super.setText3("It was a long walk but thanks to the coffee boost (super speed), you made it to work on time! Congrats!");
-		this.text4 = ("You get in the taxi and realize that you don't have enough money. Therefore, you won't make it to work on time.")";
+		this.text4 = ("You get in the taxi and realize that you don't have enough money. Therefore, you won't make it to work on time.");
 	}
 	
 	public String getText4() {
